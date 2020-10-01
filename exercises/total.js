f86adc05c231c03ad50f96329a5eb8b7a0adb92f
lauren <laurenzolivier@gmail.com>
2020-10-01 11:08:48 +0200

    solve total.js

--- exercises/total.js
+++ exercises/total.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create a `total` function that takes an array of numbers
@@ -7,13 +7,14 @@
  */
 
 // Your code:
+function total(arr) {
+  return arr.reduce((acc, cur) => acc + cur);
+} //* Begin of tests
+const assert = require("assert");
 
-//* Begin of tests
-const assert = require('assert');
-
-assert.strictEqual(typeof total, 'function');
+assert.strictEqual(typeof total, "function");
 assert.strictEqual(total.length, 1);
-assert.deepStrictEqual(total([ 1, 1, 1 ]), 3);
-assert.deepStrictEqual(total([ 10, 10, 10 ]), 30);
-assert.deepStrictEqual(total([ 24, -10, 10, 0, 0, 100 ]), 124);
+assert.deepStrictEqual(total([1, 1, 1]), 3);
+assert.deepStrictEqual(total([10, 10, 10]), 30);
+assert.deepStrictEqual(total([24, -10, 10, 0, 0, 100]), 124);
 // End of tests */
