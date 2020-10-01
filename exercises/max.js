bf03748043f1e69f23d909ac5ae5258f04849df8
lauren <lauren@MacBook-Pro-de-user.local>
2020-10-01 11:00:24 +0200

    solve max.js

--- exercises/max.js
+++ exercises/max.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create the `max` function that takes 2 number as arguments
@@ -10,13 +10,15 @@
  */
 
 // Your code:
-
+function max(nb1, nb2) {
+  return nb1 > nb2 ? nb1 : nb2;
+}
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 
-assert.strictEqual(typeof max, 'function');
+assert.strictEqual(typeof max, "function");
 assert.strictEqual(max.length, 2);
-assert.strictEqual(max.toString().includes('Math.max'), false);
+assert.strictEqual(max.toString().includes("Math.max"), false);
 assert.notStrictEqual(max, Math.max);
 assert.strictEqual(max(0, -2), 0);
 assert.strictEqual(max(-1, 10), 10);
