2261309d69ac2b801f0df77ede0d4c5f26e936ed
lauren <lauren@MacBook-Pro-de-user.local>
2020-10-01 10:55:34 +0200

    solve add.js

--- exercises/add.js
+++ exercises/add.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create a `add` function that takes 2 number arguments and add them
@@ -7,12 +7,14 @@
  */
 
 // Your code:
-
+function add(nb1, nb2) {
+  return nb1 + nb2;
+}
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 const rand = Math.random();
 
-assert.strictEqual(typeof add, 'function');
+assert.strictEqual(typeof add, "function");
 assert.strictEqual(add.length, 2);
 assert.strictEqual(add(2, 2), 4);
 assert.strictEqual(add(rand, rand), rand + rand);
