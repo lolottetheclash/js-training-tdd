5c5b8e091a0461df0ce0a71c02f0b70068077b69
lauren <laurenzolivier@gmail.com>
2020-10-01 11:02:08 +0200

    solve min.js

--- exercises/min.js
+++ exercises/min.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create the `min` function that takes 2 number as arguments
@@ -10,13 +10,15 @@
  */
 
 // Your code:
-
+function min(nb1, nb2) {
+  return nb1 > nb2 ? nb2 : nb1;
+}
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 
-assert.strictEqual(typeof min, 'function');
+assert.strictEqual(typeof min, "function");
 assert.strictEqual(min.length, 2);
-assert.strictEqual(min.toString().includes('Math.min'), false);
+assert.strictEqual(min.toString().includes("Math.min"), false);
 assert.notStrictEqual(min, Math.min);
 assert.strictEqual(min(0, -2), -2);
 assert.strictEqual(min(-1, 10), -1);
