dfd0146de6e2f9dae9de78cf6816be732b50f860
lauren <laurenzolivier@gmail.com>
2020-10-01 14:04:57 +0200

    solve cut.js

--- exercises/cut.js
+++ exercises/cut.js
@@ -11,9 +11,39 @@
  */
 
 // Your code:
+function cutFirst(str) {
+  str = str.split('');
+  str.splice(0, 2);
+  ;
+  return str.join('');
+}
+
+function cutLast(str) {
+  str = str.split('');
+  str.splice(str.length - 2, 2);
+  ;
+  return str.join('');
+}
+function cutFirstLast(str) {
+  return cutLast(cutFirst(str));
+}
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof cutFirst, "function");
+assert.strictEqual(cutFirst.length, 1);
+assert.strictEqual(cutFirst('salut'), 'lut');
+assert.strictEqual(cutFirst(''), (''));
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof cutLast, "function");
+assert.strictEqual(cutLast.length, 1);
+assert.strictEqual(cutLast('salut'), 'sal');
+assert.strictEqual(cutLast(''), (''));
+
+assert.strictEqual(typeof cutFirstLast, "function");
+assert.strictEqual(cutFirstLast.length, 1);
+assert.strictEqual(cutFirstLast('salutations'), 'lutatio');
+assert.strictEqual(cutFirstLast(''), (''));
+
+// assert.fail('You must write your own tests');
 // End of tests */
