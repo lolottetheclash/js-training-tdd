6a1b59762877b0ed621918b6364159875a34e24e
lauren <lauren@MacBook-Pro-de-user.local>
2020-10-01 10:37:55 +0200

    solve id.js

--- exercises/id.js
+++ exercises/id.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create an `id` function that takes one argument and return it
@@ -7,13 +7,15 @@
  */
 
 // Your code:
-
+function id(arg) {
+  return arg;
+}
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 
-assert.strictEqual(typeof id, 'function');
+assert.strictEqual(typeof id, "function");
 assert.strictEqual(id.length, 1);
 assert.strictEqual(id(5), 5);
-assert.strictEqual(id('pouet'), 'pouet');
+assert.strictEqual(id("pouet"), "pouet");
 assert.strictEqual(id(assert), assert);
 // End of tests */
