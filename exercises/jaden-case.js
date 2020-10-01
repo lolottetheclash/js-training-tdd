924a9aebcbf8419c77339aea1770d71e1e1f3995
lauren <laurenzolivier@gmail.com>
2020-10-01 15:26:37 +0200

    solve jaden-case.js

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -11,9 +11,23 @@
  */
 
 // Your code:
-
+function jadenCase(arg) {
+  let arr = arg.split(' ');
+  let result = arr.map(word => {
+    if (typeof word[0] === 'string') {
+      return word[0].toUpperCase() + word.slice(1, word.length);
+    }
+    return word;
+  });
+  return result.join(' ');
+}
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof jadenCase, 'function');
+assert.strictEqual(jadenCase.length, 1);
+assert.strictEqual(jadenCase("How are you ?"), "How Are You ?");
+assert.strictEqual(jadenCase("salut les futurs wilders"), "Salut Les Futurs Wilders");
+assert.strictEqual(jadenCase(""), "");
 
-assert.fail('You must write your own tests');
+// assert.fail('You must write your own tests');
 // End of tests */
