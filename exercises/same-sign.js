2017a391694f22d03a84f521b420a940f763f0d3
lauren <laurenzolivier@gmail.com>
2020-10-01 11:07:29 +0200

    solve same-sign.js

--- exercises/same-sign.js
+++ exercises/same-sign.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create the `sameSign` function that takes 2 numbers as arguments and check
@@ -8,11 +8,17 @@
  */
 
 // Your code:
-
+function sameSign(nb1, nb2) {
+  return (nb1 > 0 && nb2 > 0) ||
+    (nb1 < 0 && nb2 < 0) ||
+    (nb1 === 0 && nb2 === 0)
+    ? true
+    : false;
+}
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 
-assert.strictEqual(typeof sameSign, 'function');
+assert.strictEqual(typeof sameSign, "function");
 assert.strictEqual(sameSign.length, 2);
 assert.notStrictEqual(sameSign, Math.sameSign);
 assert.strictEqual(sameSign(-2, -1), true);
