6e14f676c6ee044bae6d9dc6224b37c76997ede0
lauren <laurenzolivier@gmail.com>
2020-10-01 11:14:39 +0200

    solve get-last.js

--- exercises/get-last.js
+++ exercises/get-last.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create a `getLast` function that takes an array or a string
@@ -8,13 +8,15 @@
  */
 
 // Your code:
-
+function getLast(arg) {
+  return arg[arg.length - 1];
+}
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 
-assert.strictEqual(getLast([ 2, 42 ]), 42);
-assert.strictEqual(getLast([ 'pouet', 4, true ]), true);
-assert.strictEqual(getLast([ getLast ]), getLast);
-assert.strictEqual(getLast('salut'), 't');
+assert.strictEqual(getLast([2, 42]), 42);
+assert.strictEqual(getLast(["pouet", 4, true]), true);
+assert.strictEqual(getLast([getLast]), getLast);
+assert.strictEqual(getLast("salut"), "t");
 assert.strictEqual(getLast([]), undefined);
 // End of tests */
